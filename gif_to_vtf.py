923c136fe52f7b14acf3f316cbce420a5d425550
Hanten Tohka <ding.ghost97@gmail.com>
2024-06-30 20:35:25 +0300

    Added support for GIF files.

---
+++ gif_to_vtf.py
@@ -0,0 +1,89 @@
+import struct
+
+
+def create_vtf_header(size, gif_frame_no):
+    # Define the values for the VTF header
+    signature = b'VTF\0'             # File signature ("VTF\0")
+    version = [7, 1]                 # Version (major, minor)
+    headerSize = 64                  # Size of the header struct (80 bytes)
+    width = size                      # Width of the largest mipmap in pixels
+    height = size                     # Height of the largest mipmap in pixels
+    flags = 0x000300                        # VTF flags (you should set appropriate flags)
+    frames = gif_frame_no                       # Number of frames, if animated
+    firstFrame = 0                   # First frame in animation (0 based)
+    padding0 = b'\0\0\0\0'           # Reflectivity padding (4 bytes)
+    reflectivity = [0x00, 0xFFFFFFFF, 0x3EC6A49F]   # Reflectivity vector (3 floats)
+    padding1 = b'\0\0\0\0'           # Reflectivity padding (4 bytes)
+    bumpmapScale = 0xFF             # Bumpmap scale (float)
+    highResImageFormat = 0x7C57FF05           # High resolution image format (int)
+    mipmapCount = 0x00                 # Number of mipmaps (unsigned char)
+    lowResImageFormat = 0x0D3F8000           # Low resolution image format (int, usually DXT1)
+    lowResImageWidth = 0             # Low resolution image width (unsigned char)
+    lowResImageHeight = 0            # Low resolution image height (unsigned char)
+    depth = 0x0D01                        # Depth of the largest mipmap in pixels (unsigned short)
+    padding2 = b'\0\0\0'             # Depth padding (3 bytes)
+    numResources = 0x0100FE                 # Number of resources this VTF has (unsigned int)
+    padding3 = b'\0\0\0\0\0\0\0\0'   # Padding for alignment (8 bytes)
+
+    vtf_header = struct.pack(
+        #'4s 2I I 2H I 2H 4x 3f 4x f i B i B B H 3x I 4x',
+        #'=4s 2I I 2H I 2H 3f f i B i B B x H 2x I',
+        '=4s 2I I 2H I 2H',
+        signature,                  # '4s' for 4-byte signature
+        version[0], version[1],     # '2I' for two 4-byte unsigned integers
+        headerSize,                 # 'I' for 4-byte unsigned integer
+        width, height,              # '2H' for two 2-byte unsigned shorts
+        flags,                      # 'I' for 4-byte unsigned integer
+        frames, firstFrame,         # '2H' for two 2-byte unsigned shorts
+        # #padding0,                   # '4s' for 4-byte padding
+        # reflectivity[0], reflectivity[1], reflectivity[2],  # '3f' for three 4-byte floats
+        # #padding1,                   # '4s' for 4-byte padding
+        # bumpmapScale,               # 'f' for 4-byte float
+        # highResImageFormat,         # 'i' for 4-byte signed integer
+        # mipmapCount,                # 'B' for 1-byte unsigned char
+        # lowResImageFormat,          # 'i' for 4-byte signed integer
+        # lowResImageWidth,           # 'B' for 1-byte unsigned char
+        # lowResImageHeight,          # 'B' for 1-byte unsigned char
+        # depth,                      # 'H' for 2-byte unsigned short
+        # #padding2,                   # '3x' for 3 bytes of padding
+        # numResources,               # 'I' for 4-byte unsigned integer
+        # #padding3                    # '8x' for 8 bytes of padding
+    )
+    hex_string = "78 00 1A 02 00 6C 1C 3F 9F A4 C6 3E 23 BD A2 3E 05 FF 57 7C 00 00 80 3F 0D 00 00 00 01 0D 00 00 00 FE 00 BA"
+    hex_bytes = bytes.fromhex(hex_string.replace(" ", ""))
+    return vtf_header + hex_bytes
+
+def create_vtf(dxt1_images, size=128):
+    VTF_HEADER_SIZE = 80  # 80 bytes for the header
+
+    # Create the VTF header
+    header = create_vtf_header(size, len(dxt1_images))
+
+    #assert len(header) == VTF_HEADER_SIZE, f"Header size is {len(header)} bytes, expected {VTF_HEADER_SIZE} bytes."
+
+    # Combine the header and the DXT1 image data
+    vtf_data = bytearray(header)
+
+    for image in dxt1_images:
+        vtf_data.extend(image)
+
+    return vtf_data
+
+
+def read_dxt1_image(file_path):
+    with open(file_path, 'rb') as f:
+        dds_content = f.read()
+        part_to_remove = bytes.fromhex(
+            "44 44 53 20 7C 00 00 00 07 10 08 00 00 04 00 00 FC 03 00 00 00 F8 07 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 20 00 00 00 04 00 00 00 44 58 54 31 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+
+        # Remove the part from dds_content
+        dds_content = dds_content[len(part_to_remove):]
+        return dds_content
+
+
+def write_vtf(vtf_data, output_vtf_filename):
+    with open(output_vtf_filename, 'wb') as f:
+        f.write(vtf_data)
+
+
+print("VTF file created successfully.")
--- main.py
+++ main.py
@@ -2,22 +2,40 @@ import os
 import subprocess
 from PIL import Image
 import argparse
+import gif_to_vtf
 
+VTF_128_ONE_FRAME_SIZE = 9
+VTF_256_ONE_FRAME_SIZE = 34
+VTF_512_ONE_FRAME_SIZE = 130
+
+vtf_size_dict = {128: VTF_128_ONE_FRAME_SIZE, 256: VTF_256_ONE_FRAME_SIZE, 512: VTF_512_ONE_FRAME_SIZE,}
 landscape = False
 parser = argparse.ArgumentParser(description="Process an input file.")
 parser.add_argument("input_path", help="Path to the input file")
 args = parser.parse_args()
-#input_image_path = "G:\\Python_Projects\\vtf_convert\\full.jpg".replace("\\","/")
+
+
+def extract_image_folder(full_path):
+    return '/'.join(full_path.split("/")[:-1]) + '/'
+def extract_image_filename(full_path):
+    return (full_path.split("/")[-1]).split(".")[0]
+
+# input_image_path = "G:\\Python_Projects\\vtf_convert\\full.jpg".replace("\\","/")
+# input_image_path = "G:\\Python_Projects\\vtf_convert\\bounce.gif".replace("\\","/")
 input_image_path = str(args.input_path).replace("\\", "/")
-image_folder = '/'.join(input_image_path.split("/")[:-1]) + '/'
-file_name = (input_image_path.split("/")[-1]).split(".")[0]
+image_folder = extract_image_folder(input_image_path)
+file_name = extract_image_filename(input_image_path)
 print(f"Input image: {input_image_path}\n")
 
-def resize_and_center_image(original_png_path):
+
+def resize_and_center_image(original_png_path, width=1024, height=1020, GIF=False):
     output_path = image_folder + "resized_" + file_name + ".png"
+    if GIF:
+        output_path = extract_image_folder(original_png_path) + "resized_" + extract_image_filename(original_png_path) + ".png"
     with Image.open(original_png_path) as img:
         # Get the original width and height of the image
         original_width, original_height = img.size
+        bigger_dimension , smaller_dimension = [width, height] if width >= height else [height, width]
 
         # Determine the scaling factor
         if original_width >= original_height:
@@ -25,32 +43,32 @@ def resize_and_center_image(original_png_path):
             global landscape
             if original_width > original_height:
                 landscape = True
-            scaling_factor = 1024 / float(original_width)
+            scaling_factor = bigger_dimension / float(original_width)
         else:
             # Image is taller than wide
-            scaling_factor = 1024 / float(original_height)
+            scaling_factor = bigger_dimension / float(original_height)
 
         # Calculate the new width and height
         temp_width = int(original_width * scaling_factor)
-        if temp_width == 1023 or temp_width == 1025:
-            temp_width = 1024
+        if temp_width == width - 1 or temp_width == width + 1:
+            temp_width = width
         temp_height = int(original_height * scaling_factor)
-        if temp_height == 1023 or temp_height == 1025:
-            temp_height = 1024
+        if temp_height == height - 1 or temp_height == height + 1:
+            temp_height = height
 
         # If image is square
         if temp_width == temp_height:
-            temp_width = 1020
+            temp_width = smaller_dimension
 
         # Resize the image
         resized_image = img.resize((temp_width, temp_height), Image.LANCZOS)
 
         # Calculate the new canvas size and paste position
-        if temp_width == 1024:
-            new_width, new_height = 1024, 1020
+        if temp_width == bigger_dimension:
+            new_width, new_height = bigger_dimension, smaller_dimension
             paste_position = ((new_width - temp_width) // 2, 0)
-        elif temp_height == 1024:
-            new_width, new_height = 1020, 1024
+        elif temp_height == bigger_dimension:
+            new_width, new_height = smaller_dimension, bigger_dimension
             paste_position = (0, (new_height - temp_height) // 2)
         else:
             raise ValueError("Input image size must be either 1024xN or Nx1024.")
@@ -70,8 +88,11 @@ def resize_and_center_image(original_png_path):
 
         return output_path
 
-def image_to_dds(resized_image_path):
+
+def image_to_dds(resized_image_path, GIF=False):
     output_path = image_folder + file_name + ".dds"
+    if GIF:
+        output_path = extract_image_folder(resized_image_path) + extract_image_filename(resized_image_path) + ".dds"
     default_executable_path = "C:\\Program Files\\NVIDIA Corporation\\NVIDIA Texture Tools\\nvcompress.exe"
     executable_path = default_executable_path
     if not os.path.exists(default_executable_path):
@@ -86,6 +107,7 @@ def image_to_dds(resized_image_path):
               f"check if it works.")
     return output_path
 
+
 def dds_to_vtf(dds_image_path):
     output_path = image_folder + file_name + ".vtf"
     hex_string = "56 54 46 00 07 00 00 00 01 00 00 00 40 00 00 00 FC 03 00 04 00 03 00 00 01 00 00 00 78 00 1A 02 00 6C 1C 3F 9F A4 C6 3E 24 BD A2 3E 05 FF 57 7C 00 00 80 3F 0D 00 00 00 01 0D 00 00 00 FE 00 01"
@@ -98,7 +120,8 @@ def dds_to_vtf(dds_image_path):
     with open(dds_image_path, "rb") as dds_file:
         dds_content = dds_file.read()
 
-    part_to_remove = bytes.fromhex("44 44 53 20 7C 00 00 00 07 10 08 00 00 04 00 00 FC 03 00 00 00 F8 07 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 20 00 00 00 04 00 00 00 44 58 54 31 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+    part_to_remove = bytes.fromhex(
+        "44 44 53 20 7C 00 00 00 07 10 08 00 00 04 00 00 FC 03 00 00 00 F8 07 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 20 00 00 00 04 00 00 00 44 58 54 31 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
 
     # Remove the part from dds_content
     dds_content = dds_content[len(part_to_remove):]
@@ -112,10 +135,65 @@ def dds_to_vtf(dds_image_path):
     print(f"\nConversion of the file to dds successful. Image saved to {output_path}")
 
 
-resized_image_path = resize_and_center_image(input_image_path)
-dds_path = image_to_dds(resized_image_path)
-dds_to_vtf(dds_path)
+def gif_to_jpg(gif_path):
+    gif = Image.open(gif_path)
 
-os.remove(resized_image_path)
-os.remove(dds_path)
-input("Press Enter to exit...")  # Wait for user input before exiting
+    frame_number = 0
+    frame_paths = []
+    while True:
+        try:
+            # Seek to the correct frame
+            gif.seek(frame_number)
+        except EOFError:
+            break  # End of sequence
+
+        # Construct the output file path
+        png_path = os.path.join(image_folder, f"{file_name}_frame_{frame_number}.jpg")
+
+        # Save the current frame as PNG
+        gif.convert("RGB").save(png_path, "PNG")
+
+        frame_number += 1
+
+        frame_paths.append(png_path)
+
+    return frame_paths
+
+
+def get_frames_summing_under_512KB(all_vtf_frames, size):
+    frame_size_KB = vtf_size_dict[size]
+    original_frame_count = len(all_vtf_frames)
+    current_frame_count = original_frame_count
+    while current_frame_count * frame_size_KB > 511:
+        current_frame_count -= 1
+    step = original_frame_count / current_frame_count
+    selected_frames = [int(i * step) for i in range(current_frame_count)]
+    selected_vtf_frames = [all_vtf_frames[i] for i in selected_frames]
+    return selected_vtf_frames
+
+
+def generate_vtf(size):
+    resized_frame_paths = [resize_and_center_image(frame_path,size, size, GIF=True) for frame_path in frame_paths]
+    dds_paths = [image_to_dds(dds_path, GIF=True) for dds_path in resized_frame_paths]
+    dxt1_images = [gif_to_vtf.read_dxt1_image(dds_path) for dds_path in dds_paths]
+    dxt_images_trimmed = get_frames_summing_under_512KB(dxt1_images, size)
+    vtf_data = gif_to_vtf.create_vtf(dxt_images_trimmed, size)
+    gif_to_vtf.write_vtf(vtf_data, image_folder + file_name + "_" + str(size) + ".vtf")
+    [os.remove(resized_frame_path) for resized_frame_path in resized_frame_paths]
+    [os.remove(dds_path) for dds_path in dds_paths]
+
+
+if input_image_path.endswith(".gif"):
+    frame_paths = gif_to_jpg(input_image_path)
+    generate_vtf(128)
+    generate_vtf(256)
+    generate_vtf(512)
+    [os.remove(frame_path) for frame_path in frame_paths]
+else:
+    resized_image_path = resize_and_center_image(input_image_path)
+    dds_path = image_to_dds(resized_image_path)
+    dds_to_vtf(dds_path)
+
+    os.remove(resized_image_path)
+    os.remove(dds_path)
+    input("Press Enter to exit...")
